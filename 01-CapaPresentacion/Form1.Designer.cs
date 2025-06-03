a8a663f66f3e791642f42ffecac57498c1b825f1
Exequiel-2023 <myeboutique2015@gmail.com>
2025-06-03 20:45:53 -0300

    Agregar archivos de proyecto.

---
+++ 01-CapaPresentacion/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace _01_CapaPresentacion
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.txt_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Usuario
+            // 
+            this.lbl_Usuario.AutoSize = true;
+            this.lbl_Usuario.Location = new System.Drawing.Point(447, 160);
+            this.lbl_Usuario.Name = "lbl_Usuario";
+            this.lbl_Usuario.Size = new System.Drawing.Size(54, 16);
+            this.lbl_Usuario.TabIndex = 0;
+            this.lbl_Usuario.Text = "Usuario";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(447, 281);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(76, 16);
+            this.lbl_Password.TabIndex = 1;
+            this.lbl_Password.Text = "Contraseña";
+            // 
+            // txt_Usuario
+            // 
+            this.txt_Usuario.Location = new System.Drawing.Point(450, 210);
+            this.txt_Usuario.Name = "txt_Usuario";
+            this.txt_Usuario.Size = new System.Drawing.Size(100, 22);
+            this.txt_Usuario.TabIndex = 2;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(450, 337);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(100, 22);
+            this.txt_Password.TabIndex = 3;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 738);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_Usuario);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Usuario);
+            this.Name = "frm_login";
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox txt_Usuario;
+        private System.Windows.Forms.TextBox txt_Password;
+    }
+}
+
